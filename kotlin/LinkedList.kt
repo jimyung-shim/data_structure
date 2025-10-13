021a6d370b3e81331d9804091ef72b72005efab2
jimyung <sjm77773@naver.com>
2025-10-13 17:22:40 +0900

    Add files via upload

--- kotlin/LinkedList.kt
+++ kotlin/LinkedList.kt
@@ -1,109 +1,222 @@
-class lnklst<T>(){
-    class node<T>(var data: T, var next:node<T>? = null){
-        override fun toString(): String {
-            return if (next != null){
-                "$data -> ${next.toString()}"
-            } else "$data"
-        }
+class LinkedList<T> : Iterable<T>, Collection<T> {
+    private var head: Node<T>? = null
+    private var tail: Node<T>? = null
+    override var size = 0
+        private set // read only로 만들기
 
-
+    override fun iterator(): Iterator<T> {
+        return LinkedListIterator(this)
     }
 
-    private var head: node<T>?=null
-    private var tail: node<T>?=null
-    private var size:Int = 0
+    override fun isEmpty(): Boolean {
+        return size == 0
+    }
 
-    fun getSize():Int=size
+    override fun contains(element: T): Boolean {
+        for (item in this)
+            if (item == element) return true
+        return false
+    }
+
+    override fun containsAll(elements: Collection<T>): Boolean {
+        for (searched in elements)
+            if (!contains(searched)) return false
+        return true
+    }
 
     override fun toString(): String {
+        if (isEmpty()) {
+            return "Empty list"
+        }
         return head.toString()
     }
 
-    private fun isEmpty(): Boolean{
-        return size==0
-    }
-
-    fun push(data:T): lnklst<T>{
-        head=node<T>(data,head)
-        if(isEmpty()){
-            tail=head
+    fun push(value: T): LinkedList<T> {
+        head = Node(value = value, next = head)
+        if (tail == null) {
+            tail = head
         }
         size++
         return this
     }
 
-    fun append(data:T):lnklst<T>{
-        if(isEmpty()) {
-            push(data)
+    fun append(value: T): LinkedList<T> {
+        // 1
+        if (isEmpty()) {
+            push(value)
             return this
-        }else{
-            val newNode=node<T>(data)
-            tail?.next=newNode
-            tail=newNode
         }
+        // 2
+        tail?.next = Node(value = value)
+        // 3
+        tail = tail?.next
         size++
         return this
     }
 
-    fun getNode(index:Int):node<T>?{
-        if(index<0&&index>=size)error("Invalid access")
-        var currentIndex=0
-        var currentNode=head
-
-        while(currentNode?.next!=null && currentIndex<index){
-            currentNode=currentNode.next
+    fun nodeAt(index: Int): Node<T>? {
+        // 1
+        var currentNode = head
+        var currentIndex = 0
+        // 2
+        while (currentNode != null && currentIndex < index) {
+            currentNode = currentNode.next
             currentIndex++
         }
         return currentNode
     }
 
-    fun insert(data:T, index:Int):node<T>{
-        if(index<0&&index>=size)error("Invalid access")
-        val newNode=node<T>(data,getNode(index)?.next)
-        getNode(index)?.next=newNode
+    fun insert(value: T, afterNode: Node<T>): Node<T> {
+        // 1
+        if (tail == afterNode) {
+            append(value)
+            return tail!!
+        }
+        // 2
+        val newNode = Node(value = value, next = afterNode.next)
+        // 3
+        afterNode.next = newNode
         size++
         return newNode
     }
 
-    fun pop():T?{
-        if(!isEmpty())size--
-        val result=head?.data
-        head=head?.next
-        return result
-    }
-
-    fun backPop():T?{
-        if(!isEmpty())size--
-        var prev=head
-        var current=head
-        var next=current?.next
-
-        while(next!=null){
-            prev=current
-            current=next
-            next=current.next
+    fun pop(): T? {
+        if (!isEmpty()) size--
+        val result = head?.value
+        head = head?.next
+        if (isEmpty()) {
+            tail = null
         }
-        val result=current?.data
-        tail=prev
-        prev?.next=null
-
         return result
     }
 
-    fun removeAfter(index:Int):T?{
-        if(index<0&&index>=size)error("Invalid access")
-        val temp =getNode(index)
-        val result=temp?.next?.data
-        if(temp?.next==tail){
-            tail=temp
-            tail?.next=null
+    fun removeHead(): T? {
+        val head = head ?: return null
+        size--
+        this.head = head.next
+        if (isEmpty())
+            this.tail = null
+        return head.value
+    }
+
+    fun removeLast(): T? {
+        // 1
+        val head = head ?: return null
+        // 2
+        if (head.next == null) return pop()
+        // 3
+        size--
+        // 4
+        var prev = head
+        var current = head
+        var next = current.next
+        while (next != null) {
+            prev = current
+            current = next
+            next = current.next
         }
-
-        temp?.next=temp.next?.next
-
-        return result
-
+        // 5
+        prev.next = null
+        tail = prev
+        return current.value
     }
 
+    fun removeAfter(node: Node<T>): T? {
+        val result = node.next?.value
+        if (node.next == tail) {
+            tail = node
+        }
+        if (node.next != null) {
+            size--
+        }
+        node.next = node.next?.next
+        return result
+    }
 
+    fun printInReverse() {
+        this.nodeAt(0)?.printInReverse()
+    }
+
+    fun getMiddle(): Node<T>? {
+        var slow = this.nodeAt(0)
+        var fast = this.nodeAt(0)
+        while (fast != null) {
+            fast = fast.next
+            if (fast != null) {
+                fast = fast.next
+                slow = slow?.next
+            }
+        }
+        return slow
+    }
+
+    private fun addInReverse(list: LinkedList<T>, node: Node<T>)
+    {
+        val next = node.next
+        if (next != null) {
+            addInReverse(list, next)
+        }
+        list.append(node.value)
+    }
+
+    fun reversed(): LinkedList<T> {
+        val result = LinkedList<T>()
+        val head = this.nodeAt(0)
+        if (head != null) {
+            addInReverse(result, head)
+        }
+        return result
+    }
+
+    private fun append(
+        result: LinkedList<T>,
+        node: Node<T>
+    ): Node<T>? {
+        result.append(node.value)
+        return node.next
+    }
+
+    fun mergeSorted(
+        otherList: LinkedList<T>
+    ): LinkedList<T> {
+        if (this.isEmpty()) return otherList
+        if (otherList.isEmpty()) return this
+        val result = LinkedList<T>()
+        var left = nodeAt(0)
+        var right = otherList.nodeAt(0)
+        while (left != null && right != null) {
+            if ((left.value as Int) < (right.value as Int)) {
+                left = append(result, left)
+            } else {
+                right = append(result, right)
+            }
+        }
+        while (left != null) {
+            left = append(result, left)
+        }
+        while (right != null) {
+            right = append(result, right)
+        }
+        return result
+    }
+}
+
+class LinkedListIterator<K> (
+    private val list: LinkedList<K>
+) : Iterator<K> {
+    private var index = 0
+    private var lastNode: Node<K>? = null
+
+    override fun next(): K {
+        if (index >= list.size) throw IndexOutOfBoundsException()
+        lastNode = if (index == 0) {
+            list.nodeAt(0)
+        } else
+            lastNode?.next
+        index++
+        return lastNode!!.value
+    }
+    override fun hasNext(): Boolean {
+        return index < list.size
+    }
 }
